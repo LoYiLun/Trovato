7e0fb491a2aa8d2de1284c75c22c1bdb6c3a9508
LoYiLun <37390225+LoYiLun@users.noreply.github.com>
2018-08-10 21:17:40 +0800

    初步對話
    20180810

---
+++ Trovato/Assets/Scripts/TalkController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class TalkController : MonoBehaviour {
+    public GameObject King;
+    public static Flowchart Talk;
+	// Use this for initialization
+	void Start () {
+        King = GameObject.Find("Cube_King");
+        Talk = GameObject.Find("對話").GetComponent<Flowchart>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+    public static bool FisrtTouchKing
+    {
+        get { return Talk.GetBooleanVariable("FisrtTouchKing"); }
+    }
+    void OnCollisionEnter(UnityEngine.Collision other)
+    {
+        if (!TalkController.FisrtTouchKing && other.transform.name == "Cube_King")
+        {
+            Flowchart.BroadcastFungusMessage("FisrtTouchKing");
+        }
+    }
+}
