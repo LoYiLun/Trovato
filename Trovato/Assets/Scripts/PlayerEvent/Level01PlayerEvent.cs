1613c67cbfa35ae7c1975cd39b89d26c0d1f74cd
LoYiLun <37390225+LoYiLun@users.noreply.github.com>
2018-09-30 15:17:31 +0800

    第一章劇情完成
    1.觸發碰撞問題並非角色彈出，目前已距離計算解決。
    2.女孩回家暫時先用"家門口觸發對話"解決。
    3.飛船口目前只找到3個點，分別是Mt.SpaceShip2、5、6，如果玩家有可能以別的地點觸發別的飛船口，需要告知，或者未來模型出來，再以模型入口為準。

--- Trovato/Assets/Scripts/PlayerEvent/Level01PlayerEvent.cs
+++ Trovato/Assets/Scripts/PlayerEvent/Level01PlayerEvent.cs
@@ -6,10 +6,15 @@ using Fungus;
 public class Level01PlayerEvent : MonoBehaviour {
     public static Flowchart main;
     public GameObject Shop;
+    public GameObject Rose;
+    public GameObject MtShip2;
+    public GameObject MtShip5;
+    public GameObject MtShip6;
     // Use this for initialization
     void Start () {
         main = GameObject.Find("MainFlowChart").GetComponent<Flowchart>();
-        
+        Rose.GetComponent<MeshRenderer>().enabled = true;
+        Rose.GetComponent<SphereCollider>().enabled = true;
     }
 	
 	// Update is called once per frame
@@ -17,6 +22,9 @@ public class Level01PlayerEvent : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.E))
         {
             Shop = GameObject.Find("Shop4");
+            MtShip2 = GameObject.Find("Mt.SpaceShip2");
+            MtShip5 = GameObject.Find("Mt.SpaceShip5");
+            MtShip6 = GameObject.Find("Mt.SpaceShip6");
             if ((Mathf.Abs(Shop.transform.position.x - transform.position.x) < 1.5f && Mathf.Abs(Shop.transform.position.z - transform.position.z) < 1.5f))
             {
                 if (Bread)
@@ -24,14 +32,55 @@ public class Level01PlayerEvent : MonoBehaviour {
                     Flowchart.BroadcastFungusMessage("BuyBread");
                 }
             }
+            if ((Mathf.Abs(MtShip2.transform.position.x - transform.position.x) < 1.5f && Mathf.Abs(MtShip2.transform.position.z - transform.position.z) < 1.5f))
+            {
+                if (Ship)
+                {
+                    Flowchart.BroadcastFungusMessage("GoShip");
+                }
+            }
+            if ((Mathf.Abs(MtShip5.transform.position.x - transform.position.x) < 1.5f && Mathf.Abs(MtShip5.transform.position.z - transform.position.z) < 1.5f))
+            {
+                if (Ship)
+                {
+                    Flowchart.BroadcastFungusMessage("GoShip");
+                }
+            }
+            if ((Mathf.Abs(MtShip6.transform.position.x - transform.position.x) < 1.5f && Mathf.Abs(MtShip6.transform.position.z - transform.position.z) < 1.5f))
+            {
+                if (Ship)
+                {
+                    Flowchart.BroadcastFungusMessage("GoShip");
+                }
+            }
+
+        }
+        if (GiveBread)
+        {
+            Rose.GetComponent<MeshRenderer>().enabled = false;
+            Rose.GetComponent<SphereCollider>().enabled = false;
         }
     }
     void OnCollisionEnter(UnityEngine.Collision other)
     {
         if(GetBread && other.transform.name == "Rose")
         {
+            Rose = GameObject.Find("Rose");
             Flowchart.BroadcastFungusMessage("GiveBread");
         }
+        if(other.transform.name == "PrinceHome6")
+        {
+            Flowchart.BroadcastFungusMessage("RoseGoHome");
+            Flowchart.BroadcastFungusMessage("SecGoHome");
+        }
+        if(other.transform.name == "GlassRepair")
+        {
+            Flowchart.BroadcastFungusMessage("FindGP");
+        }
+        /*if(Ship && Input.GetKeyDown(KeyCode.E)  && (other.transform.name == "Mt.SpaceShip2" || other.transform.name == "Mt.SpaceShip5" || other.transform.name == "Mt.SpaceShip6"))
+        {
+            Flowchart.BroadcastFungusMessage("GoShip");
+        }*/
     }
     public static bool Bread
     {
@@ -40,5 +89,13 @@ public class Level01PlayerEvent : MonoBehaviour {
     public static bool GetBread
     {
         get { return main.GetBooleanVariable("GetBread"); }
+    }
+    public static bool GiveBread
+    {
+        get { return main.GetBooleanVariable("GiveBread"); }
+    }
+    public static bool Ship
+    {
+        get { return main.GetBooleanVariable("Ship"); }
     }
 }
