63cc1c94cd14aaabc378e0d63fdf98c55a0734ef
LoYiLun <37390225+LoYiLun@users.noreply.github.com>
2018-10-01 22:22:11 +0800

    20181001
    1.先更新開頭以及盧卡斯的部分對話。
    2.因為增壓器獲得條件無法觸發，故先暫停。

---
+++ Trovato/Assets/Scripts/Level_02/BoxDestory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxDestory : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.name == "IncinerationPlant4")
+        {
+            Destroy(gameObject);
+        }
+        if (collision.transform.name == "IncinerationPlant5")
+        {
+            Destroy(gameObject);
+        }
+        if (collision.transform.name == "IncinerationPlant6")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Trovato/Assets/Scripts/PlayerEvent/Level02PlayerEvent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class Level02PlayerEvent : MonoBehaviour {
+    public Flowchart Level02main;
+    public GameObject Box_1;
+    public GameObject Box_2;
+    public GameObject Box_3;
+    public int box;
+
+    // Use this for initialization
+    void Start () {
+        box = 1;
+        Level02main = GameObject.Find("Level02Main").GetComponent<Flowchart>();
+        Box_1 = GameObject.Find("Box_1");
+        Box_2 = GameObject.Find("Box_2");
+        Box_3 = GameObject.Find("Box_3");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(!Box_1 && Box_2 && Box_3 && box ==1)
+        {
+            Flowchart.BroadcastFungusMessage("BoxDestory");
+            box = 0;
+        }
+	}
+    void OnCollisionEnter(UnityEngine.Collision other)
+    {
+        if(other.transform.name == "Lucas")
+        {
+            Flowchart.BroadcastFungusMessage("LucasTalk01");
+        }
+    }
+}
